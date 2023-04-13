489c393a9f98b6799ea5465ca88a909600e2ffc2
Ranik Santos <raniksantos53@gmail.com>
2023-04-13 15:34:35 -0300

    calculo de IMC

--- imc.js
+++ imc.js
@@ -1,4 +1,4 @@
 let p = 65;
-let a = 1.65
+let a = 1.64
 
 console.log(`O IMC é ${p / (a * a )}`);
---
+++ juros-composto.js
@@ -0,0 +1,7 @@
+
+let c = 1000;
+let i = 0.125;
+let t = 5;
+let r = 1
+
+console.log(`Resultado ${c(r + i)^t}`);
